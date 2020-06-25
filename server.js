cd4ef79828bf3626c24d9d31e41ad6896d5d8ccf
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-06-24 23:53:51 -0700

    deployment to heroku

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// this file enables the app to be run on heroku
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.stattic(__dirname + '/dist/angular-tour'));
+app.use(express.static(__dirname + '/dist/angular-tour'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angular-tour/index.html'));
+    res.sendFile(path.join(__dirname +
+        '/dist/angular-tour/index.html'));
 });
-
 app.listen(process.env.PORT || 8080);
